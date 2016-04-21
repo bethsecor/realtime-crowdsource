ac40e75eb1e91f07a220d259752495048192f340
Beth Secor <elizabeth.a.secor@gmail.com>
2016-04-21 11:11:47 -0600

    Add form

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const http = require('http');
 const express = require('express');
-
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+app.locals.polls = {};
+
 app.get('/', function (req, res){
   res.sendFile(__dirname + '/public/index.html');
+});
+
+app.post('/polls', (request, response) => {
+  // console.log(request.body.poll);
+  // console.log(request.body);
+  if (!request.body.poll) { return response.sendStatus(400); }
+  var id = generateId();
+  app.locals.polls[id] = request.body.poll;
+  // response.redirect('/polls/' + id);
 });
 
 const port = process.env.PORT || 3000;
