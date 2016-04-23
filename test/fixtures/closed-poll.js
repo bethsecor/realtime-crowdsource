796fc8b6261302cdfee3f652816c690c4d2d3e03
Beth Secor <elizabeth.a.secor@gmail.com>
2016-04-23 17:55:43 -0600

    Test poll and admin routes

---
+++ test/fixtures/closed-poll.js
@@ -0,0 +1,9 @@
+exports.validPoll = {
+  question: 'Who is your favorite Force Awakens character?',
+  options: [ 'Rey', 'Finn', 'Po', 'BB8' ],
+  share: 'on',
+  adminID: "3727717",
+  url: "https://localhost:9876/polls/123",
+  votes: {'1': 'Rey', '2': 'BB8'},
+  closed: true
+};
--- test/server-test.js
+++ test/server-test.js
@@ -1,7 +1,9 @@
 const assert = require('assert');
 const request = require('request');
 const app = require('../server');
-const fixtures = require('./fixtures');
+const newPoll = require('./fixtures/new-poll');
+const existingPoll = require('./fixtures/existing-poll');
+const closedPoll = require('./fixtures/closed-poll');
 
 describe('Server', () => {
 
@@ -60,7 +62,7 @@ describe('Server', () => {
     });
 
     it('should receive and restore data', (done) => {
-      var poll = { poll: fixtures.validPoll };
+      var poll = { poll: newPoll.validPoll };
 
       this.request.post('/polls', { form: poll }, (error, response) => {
         if (error) { done(error); }
@@ -71,13 +73,152 @@ describe('Server', () => {
     });
 
     it('should redirect the user to their new pizza', (done) => {
-      var poll = { poll: fixtures.validPoll };
+      var poll = { poll: newPoll.validPoll };
 
       this.request.post('/polls', { form: poll }, (error, response) => {
         if (error) { done(error); }
         var newPollId = Object.keys(app.locals.polls)[0];
         var newAdminId = app.locals.polls[newPollId].adminID;
         assert.equal(response.headers.location, '/polls/' + newPollId + '/admin/' + newAdminId);
+        done();
+      });
+    });
+  });
+
+  describe('GET /polls/:pollId', () => {
+    beforeEach(() => {
+      app.locals.polls.testPoll = existingPoll.validPoll;
+      app.locals.polls.closedTestPoll = closedPoll.validPoll;
+    });
+
+    it('should not return 404', (done) => {
+      var poll = app.locals.polls.testPoll;
+
+      this.request.get(`/polls/testPoll`, (error, response) => {
+        if (error) { done(error); }
+        assert.notEqual(response.statusCode, 404);
+        done();
+      });
+    });
+
+    it('should return a page that has the title of the poll', (done) => {
+      var poll = app.locals.polls.testPoll;
+
+      this.request.get(`/polls/testPoll`, (error, response) => {
+        if (error) { done(error); }
+        assert(response.body.includes(poll.question),
+               `"${response.body}" does not include "${poll.question}".`);
+        done();
+      });
+    });
+
+    it('should return a page that has the options of the poll', (done) => {
+      var poll = app.locals.polls.testPoll;
+
+      this.request.get(`/polls/testPoll`, (error, response) => {
+        if (error) { done(error); }
+        poll.options.forEach(function(option){
+          assert(response.body.includes(option),
+          `"${response.body}" does not include "${option}".`);
+        });
+        done();
+      });
+    });
+
+    it('should return a page that has the results of the poll', (done) => {
+      var poll = app.locals.polls.testPoll;
+
+      this.request.get(`/polls/testPoll`, (error, response) => {
+        if (error) { done(error); }
+        poll.options.forEach(function(option){
+          assert(response.body.includes(`${option}: 0 (0%)`),
+          `"${response.body}" does not include "${option}".`);
+        });
+        done();
+      });
+    });
+
+    it('should return a page that has a closed poll status for a closed poll', (done) => {
+      var poll = app.locals.polls.closedTestPoll;
+
+      this.request.get(`/polls/closedTestPoll`, (error, response) => {
+        if (error) { done(error); }
+        assert(response.body.includes('This poll is closed. Sad day for you.'),
+        `"${response.body}" does not include 'This poll is closed. Sad day for you'.`);
+        done();
+      });
+    });
+  });
+
+  describe('GET /polls/:pollId/admin/:adminId', () => {
+    beforeEach(() => {
+      app.locals.polls.testPoll = existingPoll.validPoll;
+      app.locals.polls.closedTestPoll = closedPoll.validPoll;
+    });
+
+    it('should not return 404', (done) => {
+      var poll = app.locals.polls.testPoll;
+
+      this.request.get(`/polls/testPoll/admin/${poll.adminID}`, (error, response) => {
+        if (error) { done(error); }
+        assert.notEqual(response.statusCode, 404);
+        done();
+      });
+    });
+
+    it('should return a page that has the results of the poll', (done) => {
+      var poll = app.locals.polls.testPoll;
+
+      this.request.get(`/polls/testPoll/admin/${poll.adminID}`, (error, response) => {
+        if (error) { done(error); }
+        poll.options.forEach(function(option){
+          assert(response.body.includes(`${option}: 0 (0%)`),
+          `"${response.body}" does not include "${option}".`);
+        });
+        done();
+      });
+    });
+
+    it('should return a page that has the status of the poll', (done) => {
+      var poll = app.locals.polls.testPoll;
+
+      this.request.get(`/polls/testPoll/admin/${poll.adminID}`, (error, response) => {
+        if (error) { done(error); }
+        assert(response.body.includes('Poll Status: Open'),
+        `"${response.body}" does not include Poll Status: Open.`);
+        done();
+      });
+    });
+
+    it('should return a page that has a close poll button', (done) => {
+      var poll = app.locals.polls.testPoll;
+
+      this.request.get(`/polls/testPoll/admin/${poll.adminID}`, (error, response) => {
+        if (error) { done(error); }
+        assert(response.body.includes('Close Poll Now'),
+        `"${response.body}" does not include Close Poll Now.`);
+        done();
+      });
+    });
+
+    it('should return a page that has a close poll in minutes', (done) => {
+      var poll = app.locals.polls.testPoll;
+
+      this.request.get(`/polls/testPoll/admin/${poll.adminID}`, (error, response) => {
+        if (error) { done(error); }
+        assert(response.body.includes('Close poll in'),
+        `"${response.body}" does not include Close poll in.`);
+        done();
+      });
+    });
+
+    it('should return a page that has a closed poll status for a closed poll', (done) => {
+      var poll = app.locals.polls.closedTestPoll;
+
+      this.request.get(`/polls/closedTestPoll/admin/${poll.adminID}`, (error, response) => {
+        if (error) { done(error); }
+        assert(response.body.includes('Poll Status: Closed'),
+        `"${response.body}" does not include Poll Status Closed.`);
         done();
       });
     });
